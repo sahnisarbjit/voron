0070aeeb6a47cc88b7394931a019017a51e84707
Sarbjit Singh <sarbjit212@gmail.com>
2024-07-01 21:12:46 +0530

    Fix install file

--- install.sh
+++ install.sh
@@ -2,8 +2,4 @@
 
 CONFIG_DIR="${HOME}/printer_data/config"
 
-git clone --recurse-submodules -j8 git@github.com:sahnisarbjit/voron.git "$CONFIG_DIR/voron"
-
 $CONFIG_DIR/klipper_z_calibration/install.sh
-
-ln -s "$CONFIG_DIR/voron/printer.cfg" "$CONFIG_DIR/printer.cfg"
